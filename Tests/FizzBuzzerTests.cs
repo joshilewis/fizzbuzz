e82692ed15f49b6c56a9ae5325431e590d12a4ca
Joshua Lewis <joshilewis@gmail.com>
2013-10-20 14:47:26 +0200

    Buzz test now passes

--- Tests/FizzBuzzerTests.cs
+++ Tests/FizzBuzzerTests.cs
@@ -86,6 +86,9 @@ namespace Tests
             if (number%3 == 0)
                 return "fizz";
 
+            if (number % 5 == 0)
+                return "buzz";
+
             return number.ToString();
         }
     }
