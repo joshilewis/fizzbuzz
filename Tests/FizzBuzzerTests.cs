33233b3b543de4961f194775950ab948b087ceb5
Joshua Lewis <joshilewis@gmail.com>
2013-10-20 14:52:26 +0200

    First Fizzbuzz test passes

--- Tests/FizzBuzzerTests.cs
+++ Tests/FizzBuzzerTests.cs
@@ -113,11 +113,16 @@ namespace Tests
 
         public virtual string Classifier(int number)
         {
+            var stringToReturn = string.Empty;
+
             if (number%3 == 0)
-                return "Fizz";
+                stringToReturn = "Fizz";
 
             if (number % 5 == 0)
-                return "Buzz";
+                stringToReturn += "Buzz";
+
+            if (!string.IsNullOrEmpty(stringToReturn))
+                return stringToReturn;
 
             return number.ToString();
         }
